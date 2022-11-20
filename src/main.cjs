751d1cd9c5a7d036ee6ca39daea3a38122d1b13e
CoasterFreakDE <mleaked@gmail.com>
2022-11-20 18:49:07 +0100

    Server Fix

--- src/main.cjs
+++ src/main.cjs
@@ -4,13 +4,9 @@ const port = parseInt(args[0]) || 3000;
 const express = require('express');
 const app = express();
 const http = require('http');
+const WebSocketServer = require('ws').Server;
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "http://127.0.0.1:5173"
-  }
-});
+const wss = new WebSocketServer({ server });
 
 var players = [];
 
@@ -18,28 +14,41 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  const playerID = socket.id
+wss.on('connection', (ws) => {
+  const playerID = generateUUID()
+  console.log(`a user connected ${playerID}`);
   players.push({id: playerID, x: 0, y: 0, rotation: 0, team: Math.floor(Math.random() * 2) + 1});
-  io.emit('players', players);
+  ws.send(JSON.stringify({event: 'players', players: players, playerID: playerID}));
 
-  socket.on('move', (data) => {
+  ws.on('message', (raw) => {
+    const data = JSON.parse(raw.data)
+    const event = data.event;
+    if (event !== 'move') return;
     const player = players.find(player => player.id === playerID);
-    player.x = data.x;
-    player.y = data.y;
-    player.rotation = data.rotation;
-    io.emit('move', player);
+    const movementData = data.player
+    player.x = movementData.x;
+    player.y = movementData.y;
+    player.rotation = movementData.rotation;
+    ws.send(JSON.stringify({event: 'move', player: player}));
   });
 
-  socket.on('disconnect', () => {
+  ws.on('disconnect', () => {
     console.log('a user disconnected');
-    const playerID = socket.id
     players = players.filter(player => player.id !== playerID);
-    io.emit('players', players);
+    ws.send(JSON.stringify({event: 'players', players: players}));
   });
 });
 
 server.listen(port, () => {
   console.log('listening on *:' + port);
 });
+
+function generateUUID() {
+  var d = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+}
